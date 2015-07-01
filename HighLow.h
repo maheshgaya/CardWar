074c631f5538facdadbd3e3a520973442e8cfaba
Mahesh Gaya <mmaheshgaya@gmail.com>
2015-07-01 09:02:16 -0500

    HighLow.h

---
+++ HighLow.h
@@ -0,0 +1,21 @@
+/*
+* author: Mahesh Gaya
+* date: 03/23/15
+* description: HighLow class
+*/
+#ifndef HIGHLOW_H
+#define HIGHLOW_H
+
+#include <iostream>
+#include "Game.h"
+using namespace std;
+
+class HighLow: public Game
+{
+public:
+	void play();
+	~HighLow();
+	
+};
+
+#endif
